c1510af5fe66b6d73178fc2f515c33e984e94f8d
Mike <tradewcs@gmail.com>
2025-10-30 13:19:49 +0200

    added hidden parameter to Animal constructor

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health: int = 100) -> None:
+    def __init__(
+        self,
+        name: str,
+        health: int = 100,
+        hidden: bool = False
+    ) -> None:
         self.name = name
         self.health = health
-        self.hidden = False
+        self.hidden = hidden
 
         self.__class__.alive.append(self)
 
